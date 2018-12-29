a95c34e702799366fc19e67159587c18bc9f0e7d
Suyatna Light <zuyatna@gmail.com>
2018-12-29 20:16:36 +0700

    Add some sample

--- config/db.js
+++ config/db.js
@@ -1,9 +1,6 @@
 const sequelize = require('sequelize')
-const conn = new sequelize('simple_api', 'root', '', {
-    
-    host: 'localhost',
-    dialect: 'mysql',
-    operatorsAliases: false
-})
+const conn = new sequelize('mysql://bbc3c590868bde:e5b0f6b2@us-cdbr-iron-east-01.cleardb.net/heroku_c76db9f7e02a1c6?reconnect=true')
+
+// 
 
 module.exports = conn
