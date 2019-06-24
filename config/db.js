94fdeb803b3f8e7709bff4ec6afc4c628704e923
suyatna <antasuy@gmail.com>
2019-06-24 12:26:51 +0700

    remove comment

--- config/db.js
+++ config/db.js
@@ -1,6 +1,4 @@
 const sequelize = require('sequelize')
 const conn = new sequelize('mysql://bbc3c590868bde:e5b0f6b2@us-cdbr-iron-east-01.cleardb.net/heroku_c76db9f7e02a1c6?reconnect=true')
 
-// 
-
 module.exports = conn
